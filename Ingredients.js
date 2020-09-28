d43d6b2998378630c4d616f9c6441927013a7798
Advay1010 <70479493+Advay1010@users.noreply.github.com>
2020-09-28 20:28:34 +0530

    Add files via upload

---
+++ Ingredients.js
@@ -0,0 +1,25 @@
+INGREDIENTS
+FOR BATTER:
+3 cup sona masuri rice3 cup sona masuri rice
+½ tsp methi / fenugreek seeds½ tsp methi / fenugreek seeds
+water, for soakingwater, for soaking
+1 cup urad dal1 cup urad dal
+2 tbsp toor dal2 tbsp toor dal
+2 tbsp chana dal2 tbsp chana dal
+1 cup poha / avalakki, rinsed1 cup poha / avalakki, rinsed
+FOR ALOO BHAJI:
+2 tbsp oil2 tbsp oil
+1 tsp mustard1 tsp mustard
+1 tsp urad dal1 tsp urad dal
+1 tsp chana dal1 tsp chana dal
+1 dried red chilli
+few curry leavesfew curry leaves
+pinch hing / asafoetidapinch hing / asafoetida
+2 chilli, finely chopped2 chilli, finely chopped
+1 inch ginger, finely chopped1 inch ginger, finely chopped
+1 onion, sliced1 onion, sliced
+¼ tsp turmeric¼ tsp turmeric
+1 tsp salt1 tsp salt
+3 potato, boiled & mashed3 potato, boiled & mashed
+2 tbsp coriander, finely chopped2 tbsp coriander, finely chopped
+2 tbsp lemon juice2 tbsp lemon juice
